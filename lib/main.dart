7af3fcc7f62861fdb4bafeb68e8114095d233cdd
hummingengineer <hummingengineer@gmail.com>
2023-12-06 21:55:04 +0900

    feat: Update colorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lime),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Tutorial Home Page'),
