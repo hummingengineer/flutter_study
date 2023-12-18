41aa32da9c2f88a97a7f046eb0fb7edc8b4d8e43
hummingengineer <hummingengineer@gmail.com>
2023-12-18 23:29:35 +0900

    feat: Center the UI

--- lib/main.dart
+++ lib/main.dart
@@ -43,18 +43,20 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          const Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
-
-          ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: const Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: const Text('Next'),
+            ),
+          ],
+        ),
       )
     );
   }
