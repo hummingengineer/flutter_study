939c388f63278ed4bc6a890986269064d5a8fb98
hummingengineer <hummingengineer@gmail.com>
2024-01-05 23:47:04 +0900

    feat: Update colorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreenAccent),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         ),
         home: const MyHomePage(),
       )
