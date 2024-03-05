cbcacba815a16f767f07543a819009526368f6bb
hummingengineer <hummingengineer@gmail.com>
2024-03-05 18:07:54 +0900

    feat: Add setState

--- first-flutter-app-tutorial/lib/main.dart
+++ first-flutter-app-tutorial/lib/main.dart
@@ -53,6 +53,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,9 +73,11 @@ class _MyHomePageState extends State<MyHomePage> {
                   label: Text('Favorites'),
                 ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,
               onDestinationSelected: (value) {
-                print('selected: $value');
+                setState(() {
+                  selectedIndex = value;
+                });
               },
             ),
           ),
