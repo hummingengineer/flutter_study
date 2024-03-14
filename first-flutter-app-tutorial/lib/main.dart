0c294c8a4ee6bfd6ced4907556dcb33d08031022
hummingengineer <hummingengineer@gmail.com>
2024-03-14 18:23:48 +0900

    feat: Add NavigationRail to LayoutBuilder

--- first-flutter-app-tutorial/lib/main.dart
+++ first-flutter-app-tutorial/lib/main.dart
@@ -113,7 +113,32 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               );
             default:
-              return const Placeholder();
+              return Row(
+                children: [
+                  SafeArea(
+                    child: NavigationRail(
+                      extended: constraints.maxWidth >= 600,
+                      destinations: const [
+                        NavigationRailDestination(
+                          icon: Icon(Icons.home),
+                          label: Text('Home'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Icons.favorite),
+                          label: Text('Favorites'),
+                        ),
+                      ],
+                      selectedIndex: selectedIndex,
+                      onDestinationSelected: (value) {
+                        setState(() {
+                          selectedIndex = value;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(child: mainArea),
+                ],
+              );
           }
         },
       ),
